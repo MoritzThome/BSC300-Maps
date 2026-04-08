63dc3b11063bc82954060ab980b8434cfb0145ea
Thome, Moritz <moritz.thome@iis.fraunhofer.de>
2026-04-08 16:55:55 +0200

    buffer and multithreading: speedup

--- generate_map.py
+++ generate_map.py
@@ -28,8 +28,8 @@ tag_file = "tag-igpsport.xml"
 
 
 cmd = (
-    "./osmosis --rbf file={input_map_file} "
-    "--mapfile-writer file={output_map_file} type=ram zoom-interval-conf=13,13,13,14,14,14 threads=1 simplification-factor=0 simplification-max-zoom=20 tag-conf-file={tag_file} "
+    "./osmosis --rbf file={input_map_file} --buffer "
+    "--mapfile-writer file={output_map_file} type=ram zoom-interval-conf=13,13,13,14,14,14 threads=2 simplification-factor=0 simplification-max-zoom=20 tag-conf-file={tag_file} "
 )
 
 if not os.path.isdir(bin_dir):
