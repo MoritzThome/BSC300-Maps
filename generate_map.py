3d78cb77bde0597682050e4a4f5ac09667c75657
Thome, Moritz <moritz.thome@iis.fraunhofer.de>
2026-04-09 09:48:56 +0200

    2 workers breaking austria, 4 threads?

--- generate_map.py
+++ generate_map.py
@@ -28,8 +28,8 @@ tag_file = "tag-igpsport.xml"
 
 
 cmd = (
-    "./osmosis --rbf file={input_map_file} workers=2 --buffer bufferCapacity=19000 "
-    "--mapfile-writer file={output_map_file} type=ram zoom-interval-conf=13,13,13,14,14,14 threads=2 simplification-factor=0 simplification-max-zoom=20 tag-conf-file={tag_file} "
+    "./osmosis --rbf file={input_map_file} workers=1 --buffer bufferCapacity=19000 "
+    "--mapfile-writer file={output_map_file} type=ram zoom-interval-conf=13,13,13,14,14,14 threads=4 simplification-factor=0 simplification-max-zoom=20 tag-conf-file={tag_file} "
 )
 
 if not os.path.isdir(bin_dir):
